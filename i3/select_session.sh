8bb9cd09b83ad4579aad66806034d9f1f9c1a278
acereca <tigerauge96@live.de>
2018-01-23 09:20:48 +0100

    premerge

--- i3/select_session.sh
+++ i3/select_session.sh
@@ -3,7 +3,7 @@
 while getopts ":oi" opt; do
   case $opt in
     o)
-        echo -e "D&D Homebrew (Atom)\nD&D Homebrew (VIM)"
+        echo -e "D&D Homebrew (Atom)\nD&D Homebrew (VIM)\nITI Mitschrieb\nReport"
       ;;
     i)
       while read LINE; do
@@ -19,12 +19,25 @@ done
 
 case $option in
     'D&D Homebrew (Atom)') 
-        i3-msg "append_layout /home/patrick/.config/i3/dnd.json; \
-        exec evince /home/patrick/gitlab/dnd/homebrew.pdf; \
-        exec atom /home/patrick/gitlab/dnd/";;
+        i3-msg "append_layout /home/patrick/.config/i3/dnd.json"
+        /usr/bin/evince /home/patrick/gitlab/dnd/homebrew.pdf &
+        atom /home/patrick/gitlab/dnd/ &
+        ;;
     'D&D Homebrew (VIM)')
-        i3-msg "append_layout /home/patrick/.config/i3/dnd_vim.json; \
-        exec evince /home/patrick/gitlab/dnd/homebrew.pdf; \
-        exec urxvt -cd /home/patrick/gitlab/dnd -e vim +NERDTree ";;
+        i3-msg "append_layout /home/patrick/.config/i3/dnd_vim.json"
+        /usr/bin/evince /home/patrick/gitlab/dnd/homebrew.pdf &
+        /usr/bin/urxvt -cd /home/patrick/gitlab/dnd -e vim +NERDTree &
+        ;;
+    'ITI Mitschrieb')
+        i3-msg "append_layout /home/patrick/.config/i3/iti.json"
+        /usr/bin/urxvt -cd /home/patrick/gitlab/ITI-WS1718 -e vim +NERDTree &
+        /usr/bin/evince /home/patrick/Downloads/uni/EiTI/EiTI2016_Skript.pdf &
+        atom /home/patrick/gitlab/ITI-WS1718/ &
+        ;;
+    'Report')
+        i3-msg "append_layout /home/patrick/.config/i3/dnd_vim.json"
+        /usr/bin/urxvt -cd /home/patrick/gitlab/intern_rep -e vim +NERDTree &
+        /usr/bin/evince /home/patrick/gitlab/intern_rep/report.pdf &
+        ;;
 esac
 
